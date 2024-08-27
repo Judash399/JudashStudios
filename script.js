cae46847739c500a1dc80ee641a522815f11b13e
Judash399 <judahandasherreddoch@gmail.com>
2024-08-27 16:36:47 -0600

    initialcommit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('colorButton');
+    
+    button.addEventListener('click', () => {
+        // Generate a random color
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
+    });
+});
